34a87da656a73f9be164113c0bf70598ec7c556b
chaubao09102005-hue <chaubao09102005@gmail.com>
2025-08-17 16:58:27 +0700

    fourthcomit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
